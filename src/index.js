0a0337b11b79f7c740ce9328f95362b063b1dabe
poyuKiryl <poyuki9@gmail.com>
2018-09-13 15:56:38 +0300

    advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,61 +1,53 @@
 module.exports = function getZerosCount(number, base) {
+    let res=0;
+    if(base!==10) {
+        let i = 2, r = 0,
+            ArrayForBaseFactors = new Map(),
+            ArrayForBaseFactorsCount = new Map(),
+            tempBase = base;
 
-    let i=2,r=[],distinctR,oneMoreArray=[],tempBase=base;
-    while(i<Math.sqrt(base)){
-        if(tempBase%i===0){
-            r.push(i);
-            tempBase/=i
-        }else{
-            i===2?i++:i+=2;
+        while (i < Math.sqrt(base)) {
+            if (tempBase % i === 0) {
+                r++;
+                ArrayForBaseFactors.set(i, r);
+                tempBase /= i
+            } else {
+                i === 2 ? i++ : i += 2;
+                r = 0;
+            }
         }
-    }
-    distinctR=distinctArray(r);
-    distinctR.forEach((el,index)=>{
-        let i=1;
-        oneMoreArray[index]=[];
-        while (+((number/(Math.pow(el,i))).toFixed())!==0){
-            let t=Math.pow(el,i);
-            oneMoreArray[index].push((number/t).toFixed());
-            i++;
+        ArrayForBaseFactors.set(tempBase, 1);
+        /*if(ArrayForBaseFactors.size===0){
+            ArrayForBaseFactors.set(base, 1)
+        }*/
+
+
+        ArrayForBaseFactors.forEach((value, key,map) => {
+            let i = 1, sum = 0;
+
+            while (Math.trunc(number / (Math.pow(key, i))) !== 0) {
+                let t = Math.pow(key, i);
+                sum += Math.trunc(number/ t);
+                i++;
+            }
+            ArrayForBaseFactorsCount.set(key, sum);
+        });
+        let minVal = Infinity, minKey;
+
+        ArrayForBaseFactorsCount.forEach((value, key) => {
+            if (value < minVal) {
+                minVal = value;
+                minKey = key;
+            }
+        });
+        return res = Math.trunc(minVal / (ArrayForBaseFactors.get(minKey)));
+    }else{
+        let currentVal=number-(number%5);
+        while(currentVal>1){
+            currentVal=(currentVal-(currentVal%5))/5;
+            res+=currentVal;
         }
-    })
+
+        return res;
+    }
 };
-
-let distinctArray = (arrArg) => {
-    return arrArg.filter((elem, pos, arr) => {
-        return arr.indexOf(elem) === pos;
-    });
-};
-
-
-
-
-
-
-
-
-
-
-
-
-/*let res=0,numberInBase='',numberInBaseArray=[],temp;
-    while(number>=base){
-        temp=number%base;
-        number=(number-temp)/base;
-        numberInBaseArray.push(temp);
-    }
-    numberInBaseArray.push(number);
-
-    for(let i=(numberInBaseArray.length-1);i>=0;i--){
-        numberInBase+=numberInBaseArray[i];
-    }
-    numberInBase=parseInt(numberInBase);
-    let currentVal=numberInBase-(numberInBase%5);
-    while(currentVal>1){
-        currentVal=(currentVal-(currentVal%5))/5;
-        res+=currentVal;
-    }
-
-
-
-    return res;*/
